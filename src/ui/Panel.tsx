b370a0f4b2e97086074f5f611a3b469cad20b57b
Dodge <93716715+AnastasiiaKrtsk@users.noreply.github.com>
2026-07-16 09:26:19 +0300

    update logic
    update logic
    styles fix

--- src/ui/Panel.tsx
+++ src/ui/Panel.tsx
@@ -8,7 +8,7 @@ export const Panel = ({ children, className }: PanelProps) => {
   return (
     <div
       className={twMerge(
-        'max-w-7xl min-w-y bg-panel px-2 py-1 md:px-3 lg:px-5 rounded border border-border ',
+        'w-full max-w-7xl bg-panel px-2 py-1 md:px-3 lg:px-5 rounded border border-border ',
         className,
       )}
     >
